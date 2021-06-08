217b2fb233020ff3a059f2f6818ca954310b6fc1
Patrick Chuong <pchuo01@yahoo.com>
2021-06-08 14:03:57 -0400

    working on design of website (how to separate page 20-80)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,27 +2,77 @@
 <hmtl lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Website</title>
+  <title>Chung King Palace Website</title>
 
 
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+<style>
+{
+    box-sizing:border-box;
+}
+html, body {
+    width:100%;
+    height:100%;
+    overflow:hidden;
+    position:fixed;
+    margin:0;
+    padding:0;
+}
+.table {
+    display:table;
+    width:100%;
+    height:100%;
+    table-layout:;
+}
 
 
-<body class="bg-gray-200">
-<nav class ="p-6 bg-white flex justify-between mb-6">
-<ul class ="flex item-center">
-<li>
-<a href="" class="p-3">Home</a>
-</li>
-<li>
-<a href=""  class="p-3">Menu</a>
-</li>
-</ul>
+.th{
+    max-width: 150px;
+    max-height: 150px;
+    overflow: hidden;
+}
 
-</nav>
-@yield('content')
+.row {
+    display:table-row;
+}
+.cell {
+    display:table-cell;
+}
+
+.header {
+    display:table-header-group;
+    border:1px solid grey;
+    height:10%;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+}
+
+li a {
+  display: block;
+  width: 60px;
+  background-color: #dddddd;
+}
+</style>
+
+<body>
+<table class='table'>
+  <tr>
+    <th style="width: 20%; vertical-align:top; text-align:center;">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </th>
+    <th style="width: 80%;">title 2</th>
+  </tr>
+</table>
+
 </body>
-
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/post', function () {
+Route::get('', function () {
     return view('post.index');
 });
