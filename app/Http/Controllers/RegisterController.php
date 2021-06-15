50f5539e12775f0d970326bcb38582bb58952446
Patrick Chuong <pchuo01@yahoo.com>
2021-06-15 14:25:25 -0400

    Making a user and signing them into website

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,9 +14,25 @@ class RegisterController extends Controller
         return view('layouts.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        dd('abc');
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('dashboard');
+
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+
 
 
 Route::get('/menu', function () {
@@ -26,3 +28,4 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
