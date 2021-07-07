b7b739ba5dcf12ff25428c91b89f065b22ed7779
Patrick Chuong <pchuo01@yahoo.com>
2021-07-07 13:20:44 -0400

    Working on query to display correct info

--- app/Http/Controllers/TotalController.php
+++ app/Http/Controllers/TotalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\orderDetail;
 use App\Models\MenuItems;
+Use DB;
 
 class TotalController extends Controller
 {
@@ -18,12 +19,15 @@ class TotalController extends Controller
     public function total(Request $request)
     {
         $total_id = $request -> total;
-        $query = orderDetail::join('menu_items','order_details.item_id','=','menu_items.id')->select('order_details.order_id','sum(menu_items.Price * order_details.quantity) as total')
+        $query = orderDetail::join('menu_items','order_details.item_id','=','menu_items.id')
+        ->select(DB::raw('sum(menu_items.Price * order_details.quantity) as total'))->groupBy('order_details.order_id')
+        //->select('order_details.order_id','menu_items.Price', 'order_details.quantity')
+        //'sum(menu_items.Price * order_details.quantity) as total'
         //->where($total_id,'=','order_details.order_id')
         ->get();
         //$query = DB::table('order_details')
         //            ->join('menu_items', 'order_details.item_id','=','menu_items.id')
-        //            ->select('order_details.id','total as (menu_items.price*order_details.quantity)')
+        //            ->select('order_details.id','total as (menu_items.Price*order_details.quantity)')
         //            ->where('order_id','=','$total_id');
 
         echo $query;
