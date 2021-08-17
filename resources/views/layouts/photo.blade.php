063499f29b14d4dddb34dff82e9b4aecaeeb7944
Patrick Chuong <pchuo01@yahoo.com>
2021-08-17 13:30:15 -0400

    Photos appear when button is pressed

--- resources/views/layouts/photo.blade.php
+++ resources/views/layouts/photo.blade.php
@@ -14,6 +14,7 @@ img
 {
   width: 300px;
   height: 200px;
+
 }
 
 .column {
@@ -25,32 +26,34 @@ img
   font-weight:bold;
 }
 
+
+
 </style>
 
 <button onclick="function1()">Lunch Combination</button>
 <button onclick="function2()">Appetizers</button>
 <button onclick="function3()">Dinner Entrees</button>
 <button onclick="function4()">Others</button>
-<div id="div1" class="row">
-  <div class="column">
-  1. Lemon Chicken<img src= "{{asset('storage/LemonChicken.jpg') }}" class="inv">
-  3. War Su Gai<img src= "{{asset('storage/WarSuGai.jpg') }}" class="inv">
-  5. Hot Braised Chicken Wings<img src= "{{asset('storage/HotBraisedChickenWings.jpg') }}" class="inv">
-  7 Chicken Sub Gum Chow Mein<img src= "{{asset('storage/ChickenSubGumChowMein.jpg') }}" class="inv">
-  11. Beef With Broccoli<img src= "{{asset('storage/BeefWithBroccoli.jpg') }}" class="inv">  
-  19. Chicken Curry<img src= "{{asset('storage/ChickenCurry.jpg') }}" class="inv">
-  21. General Tso's Chicken<img src= "{{asset('storage/GeneralTsosChicken.jpg') }}" class="inv">
-  23. Bourbon Chicken<img src= "{{asset('storage/BourbonChicken.jpg') }}" class="inv">
-  </div>
-  <div class="column">
-  2. Sweet and Sour Chicken<img src= "{{asset('storage/SweetAndSourChicken.jpg') }}" class="inv">
-  4. Sweet and Sour Shrimp<img src= "{{asset('storage/SweetAndSourShrimp.jpg') }}" class="inv">
-  6. Chicken Lo Mein<img src= "{{asset('storage/ChickenLoMein.jpg') }}" class="inv">
-  9. Green Pepper Steak<img src= "{{asset('storage/GreenPepperSteak.jpg') }}" class="inv">
-  17. Moo Shu Pork<img src= "{{asset('storage/MooShuPork.jpg') }}" class="inv">
-  20. Chung King Palace Pork<img src= "{{asset('storage/ChungKingPalacePork.jpg') }}" class="inv">
-  22. Sesame Chicken<img src= "{{asset('storage/SesameChicken.jpg') }}" class="inv">
-  24. Orange Chicken<img src= "{{asset('storage/OrangeChicken.jpg') }}" class="inv">
+<div class="row">
+    <div id="div1" class="column">
+      1. Lemon Chicken<img src= "{{asset('storage/LemonChicken.jpg') }}" class="inv">
+      3. War Su Gai<img src= "{{asset('storage/WarSuGai.jpg') }}" class="inv">
+      5. Hot Braised Chicken Wings<img src= "{{asset('storage/HotBraisedChickenWings.jpg') }}" class="inv">
+      7 Chicken Sub Gum Chow Mein<img src= "{{asset('storage/ChickenSubGumChowMein.jpg') }}" class="inv">
+      11. Beef With Broccoli<img src= "{{asset('storage/BeefWithBroccoli.jpg') }}" class="inv">  
+      19. Chicken Curry<img src= "{{asset('storage/ChickenCurry.jpg') }}" class="inv">
+      21. General Tso's Chicken<img src= "{{asset('storage/GeneralTsosChicken.jpg') }}" class="inv">
+      23. Bourbon Chicken<img src= "{{asset('storage/BourbonChicken.jpg') }}" class="inv">
+    </div>
+    <div id="div1-1" class="column">
+      2. Sweet and Sour Chicken<img src= "{{asset('storage/SweetAndSourChicken.jpg') }}" class="inv">
+      4. Sweet and Sour Shrimp<img src= "{{asset('storage/SweetAndSourShrimp.jpg') }}" class="inv">
+      6. Chicken Lo Mein<img src= "{{asset('storage/ChickenLoMein.jpg') }}" class="inv">
+      9. Green Pepper Steak<img src= "{{asset('storage/GreenPepperSteak.jpg') }}" class="inv">
+      17. Moo Shu Pork<img src= "{{asset('storage/MooShuPork.jpg') }}" class="inv">
+      20. Chung King Palace Pork<img src= "{{asset('storage/ChungKingPalacePork.jpg') }}" class="inv">
+      22. Sesame Chicken<img src= "{{asset('storage/SesameChicken.jpg') }}" class="inv">
+      24. Orange Chicken<img src= "{{asset('storage/OrangeChicken.jpg') }}" class="inv">
   </div>
 </div>
 
@@ -77,42 +80,56 @@ Small Pork Fried Rice<img src= "{{asset('storage/SmallPorkFriedRice.jpg') }}" cl
 </div>
 
 <script>
-function function1() {
   var x = document.getElementById("div1");
+  x.style.display = "none";
+  var x1 = document.getElementById("div1-1");
+  x1.style.display = "none";
+  var x2 = document.getElementById("div2");
+  x2.style.display = "none";
+  var x3 = document.getElementById("div3");
+  x3.style.display = "none";
+  var x4 = document.getElementById("div4");
+  x4.style.display = "none";
+
+function function1() {
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
+  }
+
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  } else {
+    x1.style.display = "none";
   }
 }
 
 function function2() {
-  var x = document.getElementById("div2");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  if (x2.style.display === "none") {
+    x2.style.display = "block";
   } else {
-    x.style.display = "none";
+    x2.style.display = "none";
   }
 }
 
 function function3() {
-  var x = document.getElementById("div3");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  if (x3.style.display === "none") {
+    x3.style.display = "block";
   } else {
-    x.style.display = "none";
+    x3.style.display = "none";
   }
 }
 
 function function4() {
-  var x = document.getElementById("div4");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  if (x4.style.display === "none") {
+    x4.style.display = "block";
   } else {
-    x.style.display = "none";
+    x4.style.display = "none";
   }
 }
 </script>
+
 
 </html>
 
